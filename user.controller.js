5e995fc0f2f7ced10d89518ef42936a1203811cb
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-06-22 01:28:44 +0200

    refactorizando los endpoint creando un modulo customizado

---
+++ user.controller.js
@@ -0,0 +1,20 @@
+const User = {
+    get: (req, res) => {
+        res.status(200).send('este es un chanchito')
+    },
+    list: (req, res) => {
+        res.status(200).send('Hola chanchito')
+    },
+    create: (req,res) => {
+        res.status(201).send('Creando un chanchito')
+    },
+    update: (req, res) => {
+        res.status(204).send('actualizando chanchito')
+    },
+    destroy: (req, res) => {
+        res.status(204).send('eliminando un chanchito :(')
+    }
+
+}
+
+module.exports = User
