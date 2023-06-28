83b8c861414f750d410be87337463387fabb3079
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-06-29 00:29:49 +0200

    conectando la base de datos y creando User

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Desde el terminal de VisualCode usó npm init -y, luego utilizó npm install -S express para instalar dependencias express. 
 Para arrancar la aplicación desde el terminal, usa el comando node api.js
 En el navegador utiliza localhost:3000
+He creado una cuenta para MongoDB, utilizó Atlas y AWS para versión gratuita en la nube.
--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const mongoose = require ('mongoose')
+
+mongoose.connect('mongodb+srv://onlyirina7:IrinaDan2023@cluster0.pri7c4d.mongodb.net/?retryWrites=true&w=majority')
+
+const User = mongoose.model('User', {
+    username: String,
+    edad: Number,
+})
+
+
+// La palabra reservada de new es para crear una nueva instancia
+//Para guardar en la base de datos ponemos user.save(), retorna una promesa, luego puedes agregar .then
+//Tambien para guardar en la base de datos podemos colocar la palabra reservada de await, antes de user.save
+//Si queremos imprimir en la terminal el resultado, le asignamos una constante
+const crear = async () => {
+    const user = new User({username:'Chanchito feliz', edad: 15})
+    const savedUser = await user.save()
+    console.log(savedUser)
+}
