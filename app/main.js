a79a381ab347c721ec76dbb34e99d84aa5414e26
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-07-03 21:14:01 +0200

    cargando plantilla y formulari2

--- app/main.js
+++ app/main.js
@@ -13,8 +13,21 @@ const loadInitialTemplate =() => {
     <button type="submit">Enviar</button>
     </form> 
     <ul id="user-list"></ul>   `
+    const body = document.getElementsByTagName('body')[0] //Con getElementByTagname buscamos por etiquetas
+    body.innerHTML = template
+}
+
+const addFormListener = () => {
+    const userForm = document.getElementById('user-form')
+    userForm.onSubmit = async (e) => {
+        e.preventDefault()
+        const FormData = new FormData(userForm.getAnimations('name'))
+        const data = Object.fromEntries(form.Data.entries())  //un objeto que cuente con la condicion de ser un literal
+    }
 }
 
 window.onload = () => {
     loadInitialTemplate()
+    addFormListener
 }
+
