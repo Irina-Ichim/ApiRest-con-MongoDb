08933e0a035b2390cc2f091eb1f89355e047e0f9
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-07-03 20:55:26 +0200

    agregando formulario

--- app/main.js
+++ app/main.js
@@ -1,1 +1,20 @@
-console.log('chanchito feliz')
+const loadInitialTemplate =() => {
+    const template = `
+    <h1>Usuarios</h1>
+    <form id="user-form">
+    <div>
+    <label>Nombre</label>
+    <input name="name" />
+    </div>
+    <div>
+    <label>Apellido</label>
+    <input name="lastname" />
+    </div>
+    <button type="submit">Enviar</button>
+    </form> 
+    <ul id="user-list"></ul>   `
+}
+
+window.onload = () => {
+    loadInitialTemplate()
+}
